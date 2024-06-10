a94d2ea1441df44e7d2037804ba44ded1a17c6cc
Xnattakit <xraynaikon@gmail.com>
2024-06-10 14:30:46 +0700

    add report plugin

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '8o54pk',
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
