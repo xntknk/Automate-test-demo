4c3a97dcb3aff0145a7a72799b16d29d5e94dbc8
Xnattakit <xraynaikon@gmail.com>
2024-06-26 10:06:01 +0700

    edit some config

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,19 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: '8o54pk',
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',  // Customizable directory
+    overwrite: false,
+    html: true,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Ensure to register the plugin properly
       require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
+    // Example, customize as needed
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
